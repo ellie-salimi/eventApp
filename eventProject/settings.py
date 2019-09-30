8af6bff442ab6539b836060b90c2092540b09383
Ellie <ellie.salimi@gmail.com>
2019-09-30 14:34:45 +0800

    static setup

--- eventProject/settings.py
+++ eventProject/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'eventProject/static')
+]
