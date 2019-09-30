08f96a73857a3d967e5b9aadb0fc7451b14a4ab5
Ellie <ellie.salimi@gmail.com>
2019-09-30 17:48:21 +0800

    created media structure

--- eventProject/settings.py
+++ eventProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'eventProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eventprojdb', 
+        'USER': 'postgres', 
+        'PASSWORD': '7425',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -129,3 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eventProject/static')
 ]
+
+#media folder setting
+MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from datetime import datetime
+User = get_user_model()
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    start_time = models.DateTimeField(default=datetime.now)
+    end_time = models.DateTimeField(default= datetime.now)
+    host = models.ForeignKey(User, related_name = 'hosting_events', on_delete = models.CASCADE)
+    venue = models.CharField(max_length=200)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    categories = models.ManyToManyField('Category', related_name='events')
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return str(self.title).capitalize()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return str(self.name).capitalize()
