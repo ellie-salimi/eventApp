f92c509e7090fc2bd663551f8115034e7b5192cc
Ellie <ellie.salimi@gmail.com>
2019-10-05 11:16:49 +0800

    aws

--- eventProject/settings.py
+++ eventProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,6 +27,17 @@ SECRET_KEY = 'a-g-fp)28@!_t=jbc-1nkjd^66dh^hvale%(5cryi0lwtsuq9v'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
+if 'BEANSTALK_HOST' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['BEANSTALK_HOST'])
+
+    try:
+        # Be sure your ALLOWED_HOSTS is a list NOT a tuple
+        # or .append() will fail
+        ALLOWED_HOSTS.append(socket.gethostbyname(socket.gethostname()))
+    except:
+        # silently fail as we may not be in an ECS environment
+        pass
 
 
 # Application definition
@@ -89,6 +101,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+# Use environment settings for database
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 
 # Password validation
@@ -133,6 +157,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eventProject/static')
 ]
+if 'S3_BUCKET' in os.environ:
+    # setup AWS S3 as the storage for static and media
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # define the AWS S3 bucket to use for storage
+    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+    AWS_DEFAULT_ACL = 'public-read'
 
 #media folder setting
 MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
